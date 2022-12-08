49d4a0d0caceca1b5862bfb56bb5ec661acdaea1
mochang2 <63287638+mochang2@users.noreply.github.com>
2022-12-08 22:43:25 +0900

    Update 2504 괄호의 값.py

--- BOJ/2504 괄호의 값.py
+++ BOJ/2504 괄호의 값.py
@@ -2,61 +2,105 @@
 211101
 실버2 괄호의 값
 url: https://www.acmicpc.net/problem/2504
-후기: 스택을 잘 써야 됐는데, 규칙을 결국 못 찾아서 포기했다.
+후기: 스택을 잘 써야 됐는데, 규칙을 결국 못 찾아서 포기하고 답을 봤다.
+
++) 221208 채점이 다시 되어서 수정했다.
+()]() 입력을 받으면 0이 아닌 2를 출력했었다.
+for문을 돌 때 만나는 char이 ) 또는 ]일 때 그 이전 계산 결과가 숫자이고, 그 앞에 다른 숫자나 닫는 괄호가 없을 때의 예외를 처리해줬다.
 """
 
-import sys
-string = sys.stdin.readline().strip()
-stack = []
-answer = 0
+from sys import exit, stdin
 
-def exit_with_zero():
-    print(0)
-    sys.exit(0)
+parenthesis = {
+    2: {
+        'open': '(',
+        'close': ')'
+    },
+    3: {
+        'open': '[',
+        'close': ']'
+    }
+}
 
-for s in string:
-    if s == ")":
-        tmp = 0
-        if len(stack) == 0:
-            exit_with_zero()
-        while len(stack) != 0:
-            top = stack.pop()
-            if top == "[":
-                exit_with_zero()
-            elif top == "(":
-                if tmp == 0:
-                    stack.append(2)
-                else:
-                    stack.append(tmp * 2)
-                break
-            else:
-                tmp += top
+def getInput():
+    return stdin.readline().strip()
+
+def convert(string):
+    expressions = []
+    tmp = 0
+
+    for char in string:
+        if char == parenthesis[2]['close']:
+            cannot_close_parenthesis = len(expressions) == 0
+            if cannot_close_parenthesis:
+                return getEmptyList()
+
+            tmp = 0
             
-    elif s == "]":
-        tmp = 0
-        if len(stack) == 0:
-            exit_with_zero()
-        while len(stack) != 0:
-            top = stack.pop()
-            if top == "(":
-                exit_with_zero()
-            elif top == "[":
-                if tmp == 0:
-                    stack.append(3)
+            while len(expressions) != 0:
+                last_expression = expressions.pop()
+                
+                if last_expression == parenthesis[3]['open']:
+                    return getEmptyList()
+                elif last_expression == parenthesis[2]['open']:
+                    if tmp == 0:
+                        expressions.append(2)
+                    else:
+                        expressions.append(tmp * 2)
+                    break
                 else:
-                    stack.append(tmp * 3)
-                break
-            else:
-                tmp += top
+                    not_single_close = len(expressions) != 0
+                    if not_single_close:
+                        tmp += last_expression
+                    else:
+                        return getEmptyList()
+                
+        elif char == parenthesis[3]['close']:
+            cannot_close_parenthesis = len(expressions) == 0
+            if cannot_close_parenthesis:
+                return getEmptyList()
 
-    else:
-        stack.append(s)
+            tmp = 0
+            
+            while len(expressions) != 0:
+                last_expression = expressions.pop()
+                
+                if last_expression == parenthesis[2]['open']:
+                    return getEmptyList()
+                elif last_expression == parenthesis[3]['open']:
+                    if tmp == 0:
+                        expressions.append(3)
+                    else:
+                        expressions.append(tmp * 3)
+                    break
+                else:
+                    not_single_close = len(expressions) != 0
+                    if not_single_close:
+                        tmp += last_expression
+                    else:
+                        return getEmptyList()
 
+        else:
+            expressions.append(char)
 
-for i in stack:
-    if i == '(' or i == '[':
-        exit_with_zero()
-    else:
-        answer += i
+    return expressions
+
+def getEmptyList():
+    return []
+
+def calculate(numbers):
+    result = 0
+    
+    for number in numbers:
+        if type(number) == int:
+            result += number
+        else:
+            return 0
+
+    return result
+
+parentheses = getInput()
+numbers = convert(parentheses)
+answer = calculate(numbers)
 
 print(answer)
