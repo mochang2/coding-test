e5304082ee42bcf74456c9718eace116f9578f0f
mochang2 <63287638+mochang2@users.noreply.github.com>
2023-03-19 14:14:45 +0900

    Update 불량 사용자.py

--- Programmers/3단계/불량 사용자.py
+++ Programmers/3단계/불량 사용자.py
@@ -13,6 +13,7 @@ url: https://school.programmers.co.kr/learn/courses/30/lessons/64064
 하지만 이 풀이법에는 문제가 있었다.
 3번째 단계에서 ["a123","b123","c123"], ["*123", "a***"] 와 같은 input이 들어온다면,
 mapper에서 "a***"를 가지는 key를 지워버리므로 정답인 2와 다르게 3이 출력된다.
++) 추가적으로 잘못된 게 있었는데, combination을 돌렸을 때 이중 for문일 필요가 없다.
 결국 답을 봤다.
 
 
@@ -50,8 +51,6 @@ def solution(user_ids, banned_ids):
     deleted_count = deleteUnnecessaryKeys(mapper) # 매칭되는 user_id와 banned_id의 개수가 같아 계산할 필요가 없는 key 삭제
     
     cases = set()
-    print(list(map(convertToSequentialType, mapper.values())))
-    print(len(banned_ids) - deleted_count)
     calculateValidCases(
         list(map(convertToSequentialType, mapper.values())),
         len(banned_ids) - deleted_count,
@@ -115,21 +114,79 @@ def calculateValidCases(mapper_values, counts_to_be_selected, index, cases, sele
                 counts_to_be_selected,
                 index + 1,
                 cases,
-                [*selected_user_ids, *combination]
+                [*selected_user_ids, *combination] # JS의 spread 연산
             )
 
-print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "abc1**"]), 'to be 2\n')
-print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["*rodo", "*rodo", "******"]), 'to be 2\n')
-print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "*rodo", "******", "******"]), 'to be 3\n')
-print(solution(["fradi"], ["frady"]), 'to be 0\n')
-print(solution(["frido", "frodo"], ["fr*do", "fr**o"]), 'to be 1\n')
-print(solution(["prodo", "piodi", "proda", "pradak"], ["p*od*", "prod*", "pr*da*"]), 'to be 3\n')
-print(solution(["aa","ab","cc"], ["a*", "a*", "c*"]), 'to be 1\n')
-print(solution(["a123","b123","c123"], ["*123", "a***"]), 'to be 2\n')
 
-
-## 두 번재 시도
+            
+# 두 번재 시도
+## deleteUnnecessaryKeys를 지우고, 첫 번째 풀이 calculateValidCases의 이중 for문을 지움
 ## 성공
+
+from re import compile, match
+from itertools import combinations
+
+def solution(user_ids, banned_ids):
+    mapper = initializeMapper(banned_ids)
+    matchId(mapper, user_ids)
+    
+    cases = set()
+    calculateValidCases(
+        list(map(convertToSequentialType, mapper.values())),
+        len(banned_ids),
+        0,
+        cases
+    )
+    
+    return len(cases)
+
+def initializeMapper(banned_ids):
+    mapper = dict()
+    
+    for banned_id in banned_ids:
+        if banned_id in mapper.keys():
+            mapper[banned_id]['count'] += 1
+        else:
+            mapper[banned_id] = dict()
+            mapper[banned_id]['count'] = 1
+            mapper[banned_id]['user_ids'] = []
+    
+    return mapper
+
+def matchId(mapper, user_ids):
+    for banned_id in mapper.keys():
+        regexp = str(banned_id.replace('*', '.{1}'))
+        regexp = compile('^' + regexp + '$')
+        
+        for user_id in user_ids:
+            if regexp.match(user_id):
+                mapper[banned_id]['user_ids'].append(user_id)
+
+def convertToSequentialType(mapper_value):
+    return [mapper_value['count'], mapper_value['user_ids']]
+        
+def calculateValidCases(mapper_values, counts_to_be_selected, index, cases, selected_user_ids = []):
+    if len(selected_user_ids) == counts_to_be_selected:
+        isNotDuplicatedIds = len(selected_user_ids) == len(set(selected_user_ids))
+        if isNotDuplicatedIds:
+            cases.add(str(sorted(selected_user_ids)))
+            
+        return
+    
+    [count, user_ids] = mapper_values[index]
+    for combination in combinations(user_ids, count):
+        calculateValidCases(
+            mapper_values,
+            counts_to_be_selected,
+            index + 1,
+            cases,
+            [*selected_user_ids, *combination]
+        )
+
+
+        
+## 세 번재 시도
+## 성공(두 번째 시도보다는 느림)
 
 from re import compile, match
 from itertools import permutations
